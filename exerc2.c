9894ea1e2f49b37d957d4f1d74a6991b7bc02238
Collado1234 <rennancollado@gmail.com>
2025-04-06 02:57:55 -0300

    Comentando exerc 2

--- exerc2.c
+++ exerc2.c
@@ -1,24 +1,23 @@
-// 2. Implementar o Algoritmo de Euclides para calcular o MDC
-// Descrição: O aluno deve implementar a versão iterativa do Algoritmo de Euclides para encontrar o Máximo Divisor Comum (MDC) entre dois números.
+// 2. Calcular o Maior Divisor Comum (MDC) de dois números
+// Objetivo: Implementar a versão iterativa do algoritmo para calcular o Máximo Divisor Comum.
 // Entrada:
-// Dois números inteiros A e B.
+// Dois inteiros A e B.
 
 
 // Saída:
-// O MDC entre A e B.
+// O MDC de A e B.
 
 
 // Exemplo:
-// makefile
-// CopiarEditar
 // Entrada:
 // 36 48
-
 // Saída:
 // 12
 
+
 #include<stdio.h>
 
+// Algoritmo de Euclides é uma resposta eficiente para calcular o MDC
 int main(){
     int num_scan1, num_scan2, quociente, divisor, MDC;
     printf("Digite primeiro numero: ");
@@ -27,19 +26,19 @@ int main(){
     printf("Digite segundo numero: ");
     scanf("%d", &num_scan2);
 
-    if(num_scan1 == 0 && num_scan2 == 0){
+    if(num_scan1 == 0 && num_scan2 == 0){   //Verificando possibilidade de erro
         printf("MDC indefinido para (0,0)\n");
         return -1;
     }
-    quociente = num_scan1;
+    quociente = num_scan1;   //Apenas facilitando legibilidade
     divisor = num_scan2;
-    while(divisor != 0){
+    while(divisor != 0){   
         int temp = divisor;
         divisor = quociente%divisor;
         quociente = temp;
     }
 
-    MDC = quociente;
+    MDC = quociente; //Facilitando legibilidade
 
     printf("MDC(%d,%d): %d\n",num_scan1,num_scan2,MDC);
     return 0;
