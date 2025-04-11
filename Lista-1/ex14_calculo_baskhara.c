82e809c57f90d7c04a18bc6c40ba0925f52b967b
Collado1234 <rennancollado@gmail.com>
2025-04-10 21:39:41 -0300

    Add exerc 14

---
+++ Lista-1/ex14_calculo_baskhara.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a, b, c;
+    float delta, x1, x2;
+
+    printf("Digite os coeficientes a, b e c da equação (ax^2 + bx + c = 0):\n");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a == 0) {
+        printf("Isso não é uma equação do segundo grau.\n");
+        return 1;
+    }
+
+    delta = b * b - 4 * a * c;
+
+    if (delta < 0) {
+        printf("A equação não possui raízes reais.\n");
+    } else if (delta == 0) {
+        x1 = -b / (2 * a);
+        printf("A equação possui uma raiz real: x = %.2f\n", x1);
+    } else {
+        x1 = (-b + sqrt(delta)) / (2 * a);
+        x2 = (-b - sqrt(delta)) / (2 * a);
+        printf("A equação possui duas raízes reais:\n");
+        printf("x1 = %.2f\n", x1);
+        printf("x2 = %.2f\n", x2);
+    }
+
+    return 0;
+}
