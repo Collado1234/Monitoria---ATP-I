5976b38a6d2d61ebebb877c45e6a395ab1ddd6f1
Collado1234 <rennancollado@gmail.com>
2025-04-14 18:25:36 -0300

    Add enunciado 10

--- Lista-1/ex10_converter_temperatura.c
+++ Lista-1/ex10_converter_temperatura.c
@@ -1,3 +1,13 @@
+// 10. Conversão de temperatura
+// Enunciado:
+//  Receba uma temperatura em graus Celsius e converta para Fahrenheit e Kelvin.
+//  Fórmula: F = C * 1.8 + 32
+// Fórmula: K = C + 273.15
+
+// Exemplo de entrada: Temperatura em °C: 30
+// Saída esperada: Temperatura em °F: 86 
+//   Temperatura em Kelvin: 303.15
+
 #include<stdio.h>
 
 int main (){
