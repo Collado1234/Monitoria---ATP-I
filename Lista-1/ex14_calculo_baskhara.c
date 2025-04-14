21e98ade9af100ad80be57bc901b4b359ecd947c
Collado1234 <rennancollado@gmail.com>
2025-04-14 18:51:48 -0300

    Fix: Exer14

--- Lista-1/ex14_calculo_baskhara.c
+++ Lista-1/ex14_calculo_baskhara.c
@@ -1,3 +1,22 @@
+// 14. Cálculo das raízes de uma equação do 2º grau (Bhaskara)
+// Tema: Álgebra / Resolução de equações
+//  Enunciado: Equações do segundo grau aparecem com frequência em problemas de otimização, gráficos e algoritmos.
+//  Crie um programa que receba os coeficientes A, B e C de uma equação do tipo:
+// Ax²+Bx+C = 0
+
+// e calcule as raízes reais utilizando a fórmula de Bhaskara.
+// Δ = B² - 4ac
+// Se Δ < 0, informe que não há raízes reais.1 -2
+
+
+// Se Δ=0, calcule a raiz única.
+
+
+// Se Δ>0, calcule as duas raízes reais.
+
+
+
+
 #include <stdio.h>
 #include <math.h>
 
@@ -13,7 +32,7 @@ int main() {
         return 1;
     }
 
-    delta = b * b - 4 * a * c;
+    delta = pow(b,2) - 4 * a * c;
 
     if (delta < 0) {
         printf("A equação não possui raízes reais.\n");
